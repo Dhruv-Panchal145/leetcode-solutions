1580dd768cb9f0a61f412b3d3901cb5707848fd2
dhruv-pancha145 <dhruvpanchal1345@gmail.com>
2026-08-04 09:54:20 +0530

    Time: 37 ms (78.08%), Space: 46.8 MB (34.93%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
-    int subarraySum(vector<int>& nums, int k) {
-        int n = nums.size();
+    int subarraySum(vector<int>& arr, int k) {
+        int n = arr.size();
         int count = 0;
-        for(int i = 0; i < n; i++){
-            int sum = 0;
-            for(int j = i; j < n; j++){
-                sum += nums[j];
-                if(sum == k) {
-                    count++;
-                }
+        vector<int> PrefixSum(n,0);
+
+        PrefixSum[0] = arr[0];
+        for(int i = 1; i < n; i++) {
+            PrefixSum[i] = PrefixSum[i-1] + arr[i];
+        } 
+
+        unordered_map<int, int> m; // ps, freq
+        for(int j = 0; j < n; j++) {
+            if(PrefixSum[j] == k) count++;
+
+            int val = PrefixSum[j] - k;
+            if(m.find(val) != m.end()) {
+                count += m[val];
             }
+             if(m.find(PrefixSum[j]) == m.end()) {
+                  m[PrefixSum[j]] = 0;
+             }
+             m[PrefixSum[j]]++;
         }
         return count;
     }
