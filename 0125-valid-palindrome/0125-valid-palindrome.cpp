3adc8929f7a0823ef598ef9b05fbcb054176bbc7
Dhruv-Panchal145 <dhruvpanchal1345@gmail.com>
2026-08-10 10:26:31 +0530

    Time: 6 ms (6.08%), Space: 9.9 MB (77.98%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
   
   bool isAlphanumric(char ch) {
-    if((ch >= '0' && ch <= '9') ||
+    if(ch >= '0' && ch <= '9' ||
          (tolower(ch)) >= 'a' && (tolower(ch)) <= 'z'){
             return true;
          }
@@ -13,12 +13,15 @@ public:
     bool isPalindrome(string s) {
         int st = 0, ed = s.size() - 1;
         while(st < ed) {
+            // if is check for this vaules $,3,@ 
             if(!isAlphanumric(s[st])){
                 st++; continue;
             }
+          // if is check for this vaules $,3,@ 
             if(!isAlphanumric(s[ed])) {
                 ed--; continue;
             }
+            //its check not euqal to both string then not palindeome false
           if(tolower(s[st]) != tolower(s[ed])) {
             return false;
            }
